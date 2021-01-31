71bad1a1f8769d90d08fc8b879171ac7f004f037
bpricemg <43038187+bpricemg@users.noreply.github.com>
2021-01-31 18:15:25 -0500

    Update __init__.py

--- digitsum/__init__.py
+++ digitsum/__init__.py
@@ -9,4 +9,4 @@ def exists():
 @check50.check(exists)
 def test1():
     """Sum of digits 932 is 14"""
-    check50.run("java DigitSum.java").stdin(932).stdout("[The sum of the digits is 14][14]\n").stdout(check50.EOF).exit(0)
+    check50.run("java DigitSum.java").stdin("932").stdout("[The sum of the digits is 14][14]\n").stdout(check50.EOF).exit(0)
