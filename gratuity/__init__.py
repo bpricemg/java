33951590898faa3eb09b5e0ef3770429bf782afe
bpricemg <43038187+bpricemg@users.noreply.github.com>
2021-02-01 12:22:23 -0500

    Update __init__.py

--- gratuity/__init__.py
+++ gratuity/__init__.py
@@ -15,3 +15,13 @@ def test1():
 def test2():
     """17% of $10"""
     check50.run("java Gratuity.java").stdin("10\n").stdin("17\n").stdout("1.7").stdout("11.7").exit(0)
+   
+@check50.check(exists)
+def test3():
+    """6.25% of $10"""
+    check50.run("java Gratuity.java").stdin("10\n").stdin("6.25\n").stdout("0.63").stdout("10.63").exit(0)
+
+@check50.check(exists)
+def test4():
+    """6.25% of $9.25"""
+    check50.run("java Gratuity.java").stdin("9.25\n").stdin("6.25\n").stdout("0.58").stdout("9.83").exit(0)    
