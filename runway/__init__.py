359bfbf39343f7aca4fd5e0176b25bed508b7869
bpricemg <43038187+bpricemg@users.noreply.github.com>
2021-02-02 15:33:30 -0500

    Create __init__.py

---
+++ runway/__init__.py
@@ -0,0 +1,12 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """Runway.java exists"""
+    check50.exists("Gratuity.java")
+
+@check50.check(exists)
+def test1():
+    """Speed of 60 and acceleration on 3.5 yields 514.2857142857143"""
+    check50.run("java Runway.java").stdin("60\n", prompt=False).stdin("3.5\n", prompt=False).stdout("514.2857142857143").exit(0)    
