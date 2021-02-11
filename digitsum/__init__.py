0f56280a9fe0c1d5a616be3487b51d0d0a54877d
bpricemg <43038187+bpricemg@users.noreply.github.com>
2021-02-11 12:31:29 -0500

    Update __init__.py

--- digitsum/__init__.py
+++ digitsum/__init__.py
@@ -6,12 +6,12 @@ def exists():
     """DigitSum.java exists"""
     check50.exists("DigitSum.java")
 
-@check50.check(exists)
+@check50.check()
 def test1():
     """-102 yields -3"""
     check50.run("java DigitSum.java").stdin("-102\n", prompt=False).stdout("-3").exit(0)
 
-@check50.check(exists)
+@check50.check()
 def test2():
     """402 yields 6"""
     check50.run("java DigitSum.java").stdin("402\n", prompt=False).stdout("6").exit(0)  
